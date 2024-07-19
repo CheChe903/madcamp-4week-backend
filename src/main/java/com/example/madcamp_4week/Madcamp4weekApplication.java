b59b24b756b25f5c5b55572882c43f469dfc4b49
KimJinYeongZ <cheche903@naver.com>
2024-07-19 13:57:20 +0900

    initial commit

---
+++ src/main/java/com/example/madcamp_4week/Madcamp4weekApplication.java
@@ -0,0 +1,13 @@
+package com.example.madcamp_4week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Madcamp4weekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Madcamp4weekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/madcamp_4week/Madcamp4weekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.madcamp_4week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Madcamp4weekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
